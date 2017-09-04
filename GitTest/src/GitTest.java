5bb511b8c88fe80ecd13230059baaa8a8334b3a4
Thomas Parasiuk <tparasiuk@gmail.com>
2017-09-04 10:03:12 +1000

    New test to demonstrate to Kabir

--- GitTest/src/GitTest.java
+++ GitTest/src/GitTest.java
@@ -3,6 +3,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("test");
 		
-		System.out.println("test test!!!");
+		System.out.println("test test!");
+		
+		System.out.println("new test");
 	}
 }
